f2723690e2749718a7b917a5aefd8cd5517a4a1e
SamEThibault <sam5thibault@gmail.com>
2022-08-16 22:28:46 -0400

    added navigation and first course page

--- app/__init__.py
+++ app/__init__.py
@@ -6,3 +6,7 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html", title="Note Store - Home", url=os.getenv("URL"))
+
+@app.route("/CMPE327")
+def CMPE327():
+    return render_template("CMPE327.html", title="Note Store - CMPE 327", url=os.getenv("URL"), name="CMPE 327 - Software Quality Assurance")
